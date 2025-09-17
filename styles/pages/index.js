66f6bf0b3b077ae43accbc5fb094ce1af37116ed
odiogofn <odiogofn@gmail.com>
2025-09-17 15:13:44 -0300

    Delete styles/pages directory

--- styles/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Bem-vindo ao Sistema de Palpites 🎮</h1>
-      <p>Use o menu acima para fazer login ou cadastro.</p>
-    </div>
-  );
-}
