afa03f99750d208194f85f6f4e7f4919a4302ac8
odiogofn <odiogofn@gmail.com>
2025-09-17 15:22:05 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Bem-vindo ao Sistema de Palpites 🎮</h1>
+      <h1>Bem-vinde ao Sistema de Palpites 🎮</h1>
       <p>Use o menu acima para fazer login ou cadastro.</p>
     </div>
   );
 }
+
